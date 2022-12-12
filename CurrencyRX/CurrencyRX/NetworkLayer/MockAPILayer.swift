5d715a9338c2b2c30e7d131a828f8fbd399c16cd
Mohamed Ziad <ser@sers-MacBook-Pro.local>
2022-12-12 13:35:27 -0800

    removing commnts

--- CurrencyRX/CurrencyRX/NetworkLayer/MockAPILayer.swift
+++ CurrencyRX/CurrencyRX/NetworkLayer/MockAPILayer.swift
@@ -1,44 +1,4 @@
 import UIKit
-//
-//
-//protocol URLSessionProtocol {
-//    associatedtype DataTaskType
-//    func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> DataTaskType
-//}
-//
-//
-//
-//protocol URLSessionDataTaskProtocol {
-//    func resume()
-//}
-//
-//extension URLSessionDataTask: URLSessionDataTaskProtocol {}
-//
-//class URLSessionDataTaskMock: URLSessionDataTaskProtocol {
-//    typealias CompletionHandler = URLSessionMock.CompletionHandler
-//
-//    private let completion: CompletionHandler
-//
-//    init(completion: @escaping CompletionHandler) {
-//        self.completion = completion
-//    }
-//
-//    func resume() {
-//        // create some data
-//        completion(nil, nil, nil)
-//    }
-//}
-//
-//class URLSessionMock: URLSessionProtocol {
-//    typealias CompletionHandler = (Data?, URLResponse?, Error?) -> Void
-//    // data and error can be set to provide data or an error
-//    var data: Data?
-//    var error: Error?
-//    func dataTask(with url: URL,completionHandler: @escaping CompletionHandler) -> URLSessionDataTaskMock {
-//        return URLSessionDataTaskMock(completion: completionHandler)
-//    }
-//}
-
 
 protocol URLSessionDataTaskProtocol {
   func resume()
